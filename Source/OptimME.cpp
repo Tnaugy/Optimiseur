8bda00fe2d77998f8e9994be90f6dee74e7d77d2
HUMBERT TANGUY p1906915 <tanguy.humbert@etu.univ-lyon1.fr>
2022-06-27 13:50:42 +0200

    Ajout de la ND

--- Source/OptimME.cpp
+++ Source/OptimME.cpp
@@ -23,11 +23,14 @@
 #define INTEGRAND_TYPE_SOFTRECTANGLES 4
 #define INTEGRAND_TYPE_HARDRECTANGLES 5
 
-#define INTEGRAND_TYPE_HEAVISIDE_NUMBER 262144
-#define INTEGRAND_TYPE_SOFTELLIPSES_NUMBER 524288
-#define INTEGRAND_TYPE_HARDELLIPSES_NUMBER 16384
-#define INTEGRAND_TYPE_SOFTRECTANGLES_NUMBER 16384
-#define INTEGRAND_TYPE_HARDRECTANGLES_NUMBER 16384
+#define INTEGRAND_TYPE_HEAVISIDE_2D_NUMBER 1048576
+#define INTEGRAND_TYPE_SOFTELLIPSES_2D_NUMBER 524288
+#define INTEGRAND_TYPE_HARDELLIPSES_2D_NUMBER 16384
+#define INTEGRAND_TYPE_SOFTRECTANGLES_2D_NUMBER 16384
+#define INTEGRAND_TYPE_HARDRECTANGLES_2D_NUMBER 16384
+
+#define INTEGRAND_TYPE_HEAVISIDE_3D_NUMBER 16384
+#define INTEGRAND_TYPE_SOFTELLIPSES_3D_NUMBER 16384
 
 // Declaration des variables globales
 
@@ -59,12 +62,29 @@ struct t_RectanglesStruct2D{
   double sigma[2];
 };
 
+
+struct t_Gauss3D {
+  double integral; 
+  double mu[3] ; 
+  double mxCInv[3 * 3] ; 
+};
+
+struct t_Heaviside3D {
+  double integral; 
+  double muDiscotinuity[3]; 
+  double normal[3]; 
+};
+
 // Déclaration des tableaux d'intégrandes
-extern t_Heaviside2D tab_Heaviside2D[INTEGRAND_TYPE_HEAVISIDE_NUMBER];
-extern t_GaussianStruct2D tab_SoftEllipses2D[INTEGRAND_TYPE_SOFTELLIPSES_NUMBER];
-extern t_GaussianStruct2D tab_Ellipses2D[INTEGRAND_TYPE_HARDELLIPSES_NUMBER];
-extern t_SoftRectanglesStruct2D tab_SoftRectangles2D[INTEGRAND_TYPE_SOFTRECTANGLES_NUMBER];
-extern t_RectanglesStruct2D tab_Rectangles2D[INTEGRAND_TYPE_HARDRECTANGLES_NUMBER];
+
+extern t_Heaviside2D tab_Heaviside2D[INTEGRAND_TYPE_HEAVISIDE_2D_NUMBER];
+extern t_GaussianStruct2D tab_SoftEllipses2D[INTEGRAND_TYPE_SOFTELLIPSES_2D_NUMBER];
+extern t_GaussianStruct2D tab_Ellipses2D[INTEGRAND_TYPE_HARDELLIPSES_2D_NUMBER];
+extern t_SoftRectanglesStruct2D tab_SoftRectangles2D[INTEGRAND_TYPE_SOFTRECTANGLES_2D_NUMBER];
+extern t_RectanglesStruct2D tab_Rectangles2D[INTEGRAND_TYPE_HARDRECTANGLES_2D_NUMBER];
+extern t_RectanglesStruct2D tab_Rectangles2D[INTEGRAND_TYPE_HARDRECTANGLES_2D_NUMBER];
+extern t_Gauss3D tab_Gauss3D[INTEGRAND_TYPE_SOFTELLIPSES_3D_NUMBER];
+extern t_Heaviside3D tab_Heaviside3D[INTEGRAND_TYPE_HEAVISIDE_3D_NUMBER];
 
 /* ----------- Déclaration des structures et leur "méthodes" ----------- */
 
@@ -178,29 +198,54 @@ template<int dimension>
 void initializeGaussianVectors(std::vector<MatXDynamic>* sigma,std::vector<VecXDynamic>* shift,std::vector<double>* anal,int offset,int integrandType,int gaussianSubSetSize){
   switch (integrandType) {
     case 1: // HeaviSide
-      for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
-        for (int i = 0; i < dimension; ++i) {
-            (*shift)[z-(offset*gaussianSubSetSize)][i] = tab_Heaviside2D[z].muDiscotinuity[i];  // Déplacement
-        }
-        for (int j = 0; j < dimension; ++j) {
+      if (dimension == 2) {
+        for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
+          for (int i = 0; i < dimension; ++i) {
+              (*shift)[z-(offset*gaussianSubSetSize)][i] = tab_Heaviside2D[z].muDiscotinuity[i];  // Déplacement
+          }
+          for (int j = 0; j < dimension; ++j) {
             (*sigma)[z-(offset*gaussianSubSetSize)](0, j) = tab_Heaviside2D[z].normal[j]; // Matrice SR
+          }
+          (*anal)[z-(offset*gaussianSubSetSize)] = tab_Heaviside2D[z].integral; // Valeur analytique
         }
-        (*anal)[z-(offset*gaussianSubSetSize)] = tab_Heaviside2D[z].integral; // Valeur analytique
+      }else {
+        for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
+          for (int i = 0; i < dimension; ++i) {
+              (*shift)[z-(offset*gaussianSubSetSize)][i] = tab_Heaviside3D[z].muDiscotinuity[i];  // Déplacement
+          }
+          for (int j = 0; j < dimension; ++j) {
+            (*sigma)[z-(offset*gaussianSubSetSize)](0, j) = tab_Heaviside3D[z].normal[j]; // Matrice SR
+          }
+          (*anal)[z-(offset*gaussianSubSetSize)] = tab_Heaviside3D[z].integral; // Valeur analytique
+        }
       }
     break;
     case 2: // SoftEllipses
-        for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
-
-          for (int i = 0; i < dimension; ++i) {
-              (*shift)[z-(offset*gaussianSubSetSize)][i] = tab_SoftEllipses2D[z].mu[i];  // Déplacement
-          }
-          for (int j = 0; j < dimension; ++j) {
+          if (dimension == 2) {
+            for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
               for (int i = 0; i < dimension; ++i) {
-                  (*sigma)[z-(offset*gaussianSubSetSize)](i, j) = tab_SoftEllipses2D[z].mxCInv[i + j * dimension]; // Matrice SR
+                (*shift)[z-(offset*gaussianSubSetSize)][i] = tab_SoftEllipses2D[z].mu[i];  // Déplacement
               }
+            for (int j = 0; j < dimension; ++j) {
+              for (int i = 0; i < dimension; ++i) {
+                (*sigma)[z-(offset*gaussianSubSetSize)](i, j) = tab_SoftEllipses2D[z].mxCInv[i + j * dimension]; // Matrice SR
+              }
+            }
+            (*anal)[z-(offset*gaussianSubSetSize)] = tab_SoftEllipses2D[z].integral; // Valeur analytique
+            }
+          }else{
+            for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
+              for (int i = 0; i < dimension; ++i) {
+                (*shift)[z-(offset*gaussianSubSetSize)][i] = tab_Gauss3D[z].mu[i];  // Déplacement
+              }
+            for (int j = 0; j < dimension; ++j) {
+              for (int i = 0; i < dimension; ++i) {
+                (*sigma)[z-(offset*gaussianSubSetSize)](i, j) = tab_Gauss3D[z].mxCInv[i + j * dimension]; // Matrice SR
+              }
+            }
+            (*anal)[z-(offset*gaussianSubSetSize)] = tab_Gauss3D[z].integral; // Valeur analytique
+            }
           }
-          (*anal)[z-(offset*gaussianSubSetSize)] = tab_SoftEllipses2D[z].integral; // Valeur analytique
-        }
     break;
     case 3: // HardEllipses
 
@@ -301,7 +346,7 @@ std::vector<int> randomAccessMatriceGenerator(int nbpts,int limit){
 
 /* ----------- Fonction Principale ----------- */
 template<int dimension>
-double optimPointME(std::vector<Tiles<DIM>>* v,int nbpts,std::string inputString,int niters,int nbThrow,std::string outputString,int gaussianSubSetSize,int integrandType,int limit,std::string outputStringNextStep, std::vector<std::string>* restOfTheDocument,int intervalToWrite){
+double optimPointME(std::vector<Tiles<DIM>>* v,int nbpts,std::string inputString,int niters,int nbThrow,std::string outputString,int gaussianSubSetSize,int integrandType,int limit,std::string outputStringNextStep, std::vector<std::string>* restOfTheDocument){
 
   std::default_random_engine generator;
   std::uniform_real_distribution<double> distribution(0.0,1.0);
@@ -354,6 +399,7 @@ double optimPointME(std::vector<Tiles<DIM>>* v,int nbpts,std::string inputString
       initializeGaussianVectors<dimension>(&sigma,&shift,&anal,rAMGaussiennes.at((iter_over_pointset % (( total_N_integrands / gaussianSubSetSize )))),integrandType,gaussianSubSetSize);
       mseOfAPointsetOnAllGaussian(&points,&sigma,&shift,&anal,gaussianSubSetSize,tabPtsValGauss,integrandType);
       for (int i_pts = 0; i_pts < nbpts-limit; i_pts++) {
+        std::cout <<  rAM.at(i_pts) << std::endl;
           #pragma omp parallel for
           for (int i_pt_in_tile = 0; i_pt_in_tile < nbThrow; i_pt_in_tile++) {
             generator.seed((i_pt_in_tile*1234+5678)+std::chrono::system_clock::now().time_since_epoch().count());
@@ -362,29 +408,41 @@ double optimPointME(std::vector<Tiles<DIM>>* v,int nbpts,std::string inputString
                   mseTab[i_pt_in_tile].point[currentDim-1] = points[rAM.at(i_pts)][currentDim-1];
             }
                   double rand = distribution(generator);
-             (mseTab[i_pt_in_tile].point)[0] = (*(v->at(rAM.at(i_pts)).getPreviousRefPoint()))[0] +  ( ( ( (double)(i_pt_in_tile/8) * v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(1)[0] ) ) / 8.0 ) +   rand * ( (v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(1)[0] / 8.0) + ( ( ( (double)(i_pt_in_tile/8) *v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(2)[0]) ) / 8.0 ) + rand * ( (v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(2)[0]) / 8.0));
-                        rand = distribution(generator);
-             (mseTab[i_pt_in_tile].point)[1] = (*(v->at(rAM.at(i_pts)).getPreviousRefPoint()))[1] +  ( ( ( (double)(i_pt_in_tile/8) * v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(1)[1] ) ) / 8.0 ) +    rand * ( (v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(1)[1] / 8.0) + ( ( ( (double)(i_pt_in_tile/8) *v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(2)[1]) ) / 8.0 ) + rand * ( (v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(2)[1]) / 8.0));
+            for (int i = 0; i < dimension; i++) {
+              double shiftInOneDimension = 0.0;
+              for (int j = 1; j <= dimension; j++) {
+                rand =  distribution(generator);
+               shiftInOneDimension += 
+              ( ( ( (double)(i_pt_in_tile/8) * v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(j)[i] ) ) / 8.0 ) 
+              + rand * ( (v->at(rAM.at(i_pts)).getPreviousDirectionnalVector(j)[i] / 8.0));
+
+              }
+
+              (mseTab[i_pt_in_tile].point)[i] = (*(v->at(rAM.at(i_pts)).getPreviousRefPoint()))[i] + shiftInOneDimension; 
+
+            }
           mseTab[i_pt_in_tile].apportOfNewPoint = recalculateGaussianValueAllGauss(points[rAM.at(i_pts)],mseTab[i_pt_in_tile].point, &sigma,&shift,tabPtsValGauss,gaussianSubSetSize,nbpts,&anal,integrandType);
+            //std::cout << "X ==> " <<  (mseTab[i_pt_in_tile].point)[0] << " et Y ==> " << (mseTab[i_pt_in_tile].point)[1] << std::endl;
           }
+
+              //std::cout <<  mseTab[0].apportOfNewPoint << std::endl;
           newPointHolder<dimension> theChosenOne = *std::min_element(mseTab+0,mseTab+nbThrow,compareTwoNewPointHolder<dimension>);
           if (theChosenOne.apportOfNewPoint < tabPtsValGauss[gaussianSubSetSize]) {
+
+            
             double delta = fabs(theChosenOne.apportOfNewPoint - prevMSE);
-            std::cout << outputString << " iter=" << iter_over_pointset << " npts=" << nbpts << " MSE : " << initialSE << " -> " << theChosenOne.apportOfNewPoint << " \t delta : "<< delta << std::endl;
+            if (rAM.at(i_pts) == 0) {
+              std::cout << outputString << " Iteration " << iter_over_pointset << " : " << " MSE : " << initialSE << " -> " << theChosenOne.apportOfNewPoint << " \t delta : "<< delta << std::endl;
+            }
             prevMSE = theChosenOne.apportOfNewPoint;
             tabPtsValGauss[gaussianSubSetSize] = theChosenOne.apportOfNewPoint;
             changeAllValueGaussTab(points[theChosenOne.index],theChosenOne.point, &sigma,&shift, tabPtsValGauss,gaussianSubSetSize,nbpts,integrandType);
-            points[theChosenOne.index][0] = theChosenOne.point[0];
-            points[theChosenOne.index][1] = theChosenOne.point[1];
+            for (int i = 0; i < dimension; i++) {
+            points[theChosenOne.index][i] = theChosenOne.point[i];
+            }
             injectSP(v,&points);
           }
       }
-
-
-      if (iter_over_pointset % intervalToWrite == 0 ) {
-         exportTiles(v,outputString);   
-      }
-
     }
 
     exportTiles(v,outputString);
@@ -411,10 +469,10 @@ int main(int argc, char const *argv[]) {
   std::string outputStringMSE ="MSE.dat";
   int limit = 1;
   std::vector<std::string>* restOfTheDocument = new std::vector<std::string>;
-  int intervalToWrite = 10;
-  /* ----------- Fin Initialisation des variables ----------- */
 
-  // =========== Début CLI11 Configuration =========== //
+  //[> ----------- Fin Initialisation des variables ----------- <]
+
+  //// =========== Début CLI11 Configuration =========== //
 
       CLI::App app { "OptimME" };
 
@@ -428,47 +486,72 @@ int main(int argc, char const *argv[]) {
       app.add_option("--integrandType",integrandType,"Type of the integrand to compute MSE : 1|-> HeaviSide, 2|-> SoftEllipses, 3|-> HardEllipses, 4|-> SoftRectangles, 5|-> HardRectangles. Default: "+std::to_string(integrandType));
       app.add_option("-l,--limit",limit,"The limit number behind which we don't modify the points. If left by default, will take every point. Default: "+std::to_string(limit));
       app.add_option("--outputNextStep",outputStringNextStep,"Path to output file fot next step, i.e. with all the 729 points. If left by default, it will not write it. default: "+outputStringNextStep);
-      app.add_option("--writingInterval",intervalToWrite,"Defines at which interval should the pointset be written. Default: "+std::to_string(intervalToWrite));
+
       CLI11_PARSE(app, argc, argv)
 
 // Initialisation du nombre d'integrandes en fonction du type d'integrande choisi
 
       switch (integrandType) {
         case INTEGRAND_TYPE_HEAVISIDE:
-          total_N_integrands = INTEGRAND_TYPE_HEAVISIDE_NUMBER;
-          break;
+          switch (DIM) {
+            case 2:
+              total_N_integrands = INTEGRAND_TYPE_HEAVISIDE_2D_NUMBER;
+              break;
+            case 3:
+              total_N_integrands = INTEGRAND_TYPE_HEAVISIDE_3D_NUMBER;
+              break;
+            default:
+              std::cerr << " L'integrande specifiee n'est pas disponible en dimension " << std::to_string(DIM) << std::endl;
+              break;
+          }
         case INTEGRAND_TYPE_SOFTELLIPSES:
-          total_N_integrands = INTEGRAND_TYPE_SOFTELLIPSES_NUMBER;
+          switch (DIM) {
+            case 2:
+              total_N_integrands = INTEGRAND_TYPE_SOFTELLIPSES_2D_NUMBER;
+              break;
+            case 3:
+              total_N_integrands = INTEGRAND_TYPE_SOFTELLIPSES_3D_NUMBER;
+              break;
+            default:
+              std::cerr << " L'integrande specifiee n'est pas disponible en dimension " << std::to_string(DIM) << std::endl;
+              break;
+          }
           break;
         case INTEGRAND_TYPE_HARDELLIPSES:
-          total_N_integrands = INTEGRAND_TYPE_HARDELLIPSES_NUMBER;
+          total_N_integrands = INTEGRAND_TYPE_HARDELLIPSES_2D_NUMBER;
           break;
         case INTEGRAND_TYPE_SOFTRECTANGLES:
-          total_N_integrands = INTEGRAND_TYPE_SOFTRECTANGLES_NUMBER;
+          total_N_integrands = INTEGRAND_TYPE_SOFTRECTANGLES_2D_NUMBER;
           break;
         case INTEGRAND_TYPE_HARDRECTANGLES:
-          total_N_integrands = INTEGRAND_TYPE_HARDRECTANGLES_NUMBER;
+          total_N_integrands = INTEGRAND_TYPE_HARDRECTANGLES_2D_NUMBER;
           break;
         default:
         std::cerr << "L'integrande specifiee n'est pas disponible." << '\n';
         return -1;
       }
 
-      // =========== Fin CLI11 Configuration =========== //
-                                          /*****/
-      // =========== Début OpenMP Configuration =========== //
+      //// =========== Fin CLI11 Configuration =========== //
+                                          //[>***<]
+      //// =========== Début OpenMP Configuration =========== //
 
-      omp_set_dynamic(0);
-      omp_set_num_threads(nbThreads);
+    omp_set_dynamic(0);
+    omp_set_num_threads(nbThreads);
 
-      // =========== Fin OpenMP Configuration =========== //
+      //// =========== Fin OpenMP Configuration =========== //
 
-        std::vector<Tiles<DIM>>* v = importTiles<DIM>(inputString,nbpts,restOfTheDocument);
-        double val = optimPointME<DIM>(v,nbpts,inputString,niters,nbThrow,outputString,gaussianSubSetSize,integrandType,limit-1,outputStringNextStep,restOfTheDocument,intervalToWrite);
+    std::vector<Tiles<DIM>>* v = importTiles<DIM>(inputString,nbpts,restOfTheDocument);
+
+        //for (auto i : (*v)) {
+          //std::cout << i << std::endl;
+        //}
+    double val = optimPointME<DIM>(v,nbpts,inputString,niters,nbThrow,outputString,gaussianSubSetSize,integrandType,limit-1,outputStringNextStep,restOfTheDocument);
         // =========== Ecriture de l'erreur associée à un pointset  =========== //
         // std::cout << val << '\n';
-        if (outputStringMSE.compare("MSE.dat") != 0) {
-          exportValue(nbpts,val,outputStringMSE);
-        }
+    if (outputStringMSE.compare("MSE.dat") != 0) {
+      exportValue(nbpts,val,outputStringMSE);
+    }
+        
+        std::cout << DIM << std::endl;
   return 0;
 }
